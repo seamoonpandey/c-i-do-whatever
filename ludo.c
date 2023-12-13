a2e129b02cedb82e9a097ece99ce3d46008a8dec
samrat-dotel <samratdotel7264@gmail.com>
2023-12-13 15:01:18 +0545

    random success

--- ludo.c
+++ ludo.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 int main() {
     int turn = 0;
-    char player[3][20]; 
-    int finalscore[3] = {0}; 
+    char player[3][20];
+    int finalscore[3] = {0};
+
+    // Seed the random number generator with the current time
+    srand((unsigned int)time(NULL));
 
     for (int i = 0; i < 3; i++) {
         printf("Enter player %d's name: ", i + 1);
@@ -17,24 +21,24 @@ int main() {
         printf("%s got %d \n", player[turn % 3], current);
         sleep(1);
 
-		finalscore[turn % 3] += current;
+        finalscore[turn % 3] += current;
 
         if (current == 6) {
             printf("Wow! %s rolled a 6 and gets another chance.\n", player[turn % 3]);
         } else {
             printf("%s's current score is now %d \n", player[turn % 3], finalscore[turn % 3]);
             printf("Loading next turn .... \n");
-            sleep(2);
+            sleep(1.5);
             turn += 1;
         }
     }
 
     if (finalscore[0] >= 100) {
-        printf("%s is the fucking winner", player[0]);
+        printf("%s is the winner", player[0]);
     } else if (finalscore[1] >= 100) {
-        printf("%s is the fucking winner", player[1]);
+        printf("%s is the winner", player[1]);
     } else {
-        printf("%s is the sucking winner", player[2]);
+        printf("%s is the winner", player[2]);
     }
 
     return 0;
