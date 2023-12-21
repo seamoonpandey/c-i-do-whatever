9d10eba9b99a544d98f8c0c4a8cbb495bdfee19d
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-21 08:05:18 +0545

    add some more beautify and clean

--- saapsidi.c
+++ saapsidi.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 
 #define NUM_SNAKES 5
 #define NUM_LADDERS 5
 #define DEFAULT_FINAL_SQUARE 100
+
+// ANSI escape codes for text color
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 void displayBoard(int numPlayers, char player[][20], int position[])
 {
@@ -45,14 +55,14 @@ void playGame(int numPlayers, int finalSquare)
 
         int current = rand() % 6 + 1;
         printf("Rolled a %d \n", current);
-        sleep(1);
+        sleep(1000);
 
         // Check for snakes and ladders
         for (int i = 0; i < NUM_SNAKES; i++)
         {
             if (position[turn % numPlayers] == snakes[i][0])
             {
-                printf("Oh no! Encountered a snake! Sliding down to %d.\n", snakes[i][1]);
+                printf("%sOh no! Encountered a snake! Sliding down to %d.%s\n", ANSI_COLOR_RED, snakes[i][1], ANSI_COLOR_RESET);
                 position[turn % numPlayers] = snakes[i][1]; // Corrected line
             }
         }
@@ -61,7 +71,7 @@ void playGame(int numPlayers, int finalSquare)
         {
             if (position[turn % numPlayers] == ladders[i][0])
             {
-                printf("Yay! Found a ladder! Climbing up to %d.\n", ladders[i][1]);
+                printf("%sYay! Found a ladder! Climbing up to %d.%s\n", ANSI_COLOR_GREEN, ladders[i][1], ANSI_COLOR_RESET);
                 position[turn % numPlayers] = ladders[i][1];
             }
         }
@@ -72,20 +82,21 @@ void playGame(int numPlayers, int finalSquare)
         // Check if the player reaches or passes the final square
         if (position[turn % numPlayers] >= finalSquare)
         {
-            printf("Congratulations! %s reached the final square! They are the winner!\n", player[turn % numPlayers]);
+            printf("%sCongratulations! %s reached the final square! They are the winner!%s\n", ANSI_COLOR_GREEN, player[turn % numPlayers], ANSI_COLOR_RESET);
             displayBoard(numPlayers, player, position);
             return;
         }
 
-        printf("%s's current position is now %d \n", player[turn % numPlayers], position[turn % numPlayers]);
+        printf("%s%s's current position is now %d %s\n", ANSI_COLOR_YELLOW, player[turn % numPlayers], position[turn % numPlayers], ANSI_COLOR_RESET);
 
         // Check for collisions with other players
         for (int i = 0; i < numPlayers; i++)
         {
             if (i != turn % numPlayers && position[i] == position[turn % numPlayers])
             {
-                printf("Oh no! Collision with %s! %s eliminates %s, sending them back to square 1.\n", player[i], player[turn % numPlayers], player[i]);
-                position[i] = 1; // The latest player eliminates the previous one
+                printf("%sOh no! Collision with %s! %s eliminates %s, sending them back to square 1.%s\n", ANSI_COLOR_RED, player[i], player[turn % numPlayers], player[i], ANSI_COLOR_RESET);
+                position[i] = 1; 
+                printf("%s gets another chance",player[turn % numPlayers]);
             }
         }
 
@@ -95,13 +106,13 @@ void playGame(int numPlayers, int finalSquare)
             consecutiveSixCount[turn % numPlayers]++;
             if (consecutiveSixCount[turn % numPlayers] == 3)
             {
-                printf("Oh no! %s rolled three consecutive 6s and gets a penalty of -6!\n", player[turn % numPlayers]);
-                position[turn % numPlayers] -= 12;
+                printf("%sOh no! %s rolled three consecutive 6s and gets a penalty of -6!%s\n", ANSI_COLOR_RED, player[turn % numPlayers], ANSI_COLOR_RESET);
+                position[turn % numPlayers] -= 6;
                 consecutiveSixCount[turn % numPlayers] = 0;
             }
             else
             {
-                printf("Wow! %s rolled a 6 and gets another chance.\n", player[turn % numPlayers]);
+                printf("%sWow! %s rolled a 6 and gets another chance.%s\n", ANSI_COLOR_GREEN, player[turn % numPlayers], ANSI_COLOR_RESET);
                 continue; // Bonus chance for rolling a 6
             }
         }
@@ -112,8 +123,8 @@ void playGame(int numPlayers, int finalSquare)
 
         displayBoard(numPlayers, player, position);
 
-        printf("%s is rolling now .... \n", player[turn % numPlayers]);
-        sleep(1.5);
+        printf("%s%s is rolling now .... %s\n", ANSI_COLOR_BLUE, player[turn % numPlayers], ANSI_COLOR_RESET);
+        sleep(15000);
         turn += 1;
     }
 }
