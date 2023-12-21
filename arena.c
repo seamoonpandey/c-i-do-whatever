76ebbf8e6b22608e929bf53b1b0ae57e74a20800
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-21 08:14:15 +0545

    add the config

--- arena.c
+++ arena.c
@@ -2,23 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-#ifdef _WIN32
-    // For Windows
-    #define CLEAR_SCREEN "cls"
-#else
-    // For Unix/Linux
-    #define CLEAR_SCREEN "clear"
-#endif
-
-// ANSI escape codes for text color
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#include "config.h"
 
 typedef struct {
     char name[20];
